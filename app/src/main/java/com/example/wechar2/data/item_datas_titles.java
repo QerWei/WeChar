aa85044001e4f367ece055e11e9cbb6838f5bc6d
Wei <QerWei@users.noreply.github.com>
2020-11-21 18:03:54 +0800

    RecyclerView_FriendsTab
    Add a recycleview on fragment_tab_friends.

---
+++ app/src/main/java/com/example/wechar2/data/item_datas_titles.java
@@ -0,0 +1,22 @@
+package com.example.wechar2.data;
+
+import com.example.wechar2.R;
+
+public class item_datas_titles {
+    public static String[] titles = {
+            "1. 白雪公主",
+            "2. 仙蒂公主（灰姑娘）",
+            "3. 爱洛公主（睡美人）",
+            "4. 爱丽儿",
+            "5. 贝尔",
+            "6. 茉莉公主",
+            "7. 宝嘉康蒂公主",
+            "8. 花木兰",
+            "9. 蒂安娜",
+            "10. 乐佩公主（长发公主）",
+            "11. 梅莉达公主",
+            "12. 艾莎"
+    };
+
+
+}
--- app/src/main/java/com/example/wechar2/friendsFragment.java
+++ app/src/main/java/com/example/wechar2/friendsFragment.java
@@ -2,11 +2,23 @@ package com.example.wechar2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.wechar2.adapter.RecyclerViewAdapter;
+import com.example.wechar2.data.item_data_bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.wechar2.data.item_datas_icons;
+import com.example.wechar2.data.item_datas_titles;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +35,10 @@ public class friendsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private List<item_data_bean> mData;
+    private View view;
+
 
     public friendsFragment() {
         // Required empty public constructor
@@ -56,9 +72,45 @@ public class friendsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_friends, container, false);
+
+        view = inflater.inflate(R.layout.fragment_tab_friends,container,false);
+        //数据准备
+        initData();
+
+        //将数据与布局绑定
+        initView();
+
+//        return inflater.inflate(R.layout.fragment_tab_friends, container, false);
+        return view;
     }
+
+    private void initView() {
+        RecyclerView recyclerView = view.findViewById(R.id.fri_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(),mData);
+
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setHasFixedSize(true);
+
+
+    }
+
+    private void initData() {
+        mData = new ArrayList<>();
+        //将item_datas_icons和item_datas_titles中的数据放入mData中
+        for (int i = 0; i < item_datas_icons.icons.length && i < item_datas_titles.titles.length; i++){
+            //分别从两个存放数据的类中获取数据
+            item_data_bean itemdata = new item_data_bean();
+            itemdata.icons = item_datas_icons.icons[i];
+            itemdata.title = item_datas_titles.titles[i];
+            //将集合添加到列表
+            mData.add(itemdata);
+        }
+    }
+
+
 }
